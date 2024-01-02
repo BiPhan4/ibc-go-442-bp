ea5b61418103bac035ff9275cc9e4b6b264a64c9
BiPhan4 <biphan374@gmail.com>
2024-01-02 11:57:47 -0500

    add logger

---
+++ modules/apps/27-interchain-accounts/logger/logger.go
@@ -0,0 +1,41 @@
+package logger
+
+import (
+	"log"
+	"os"
+)
+
+var (
+	InfoLogger  *log.Logger
+	ErrorLogger *log.Logger
+)
+
+func InitLogger() {
+	path := "logs/"
+
+	// Create directory if it doesn't exist
+	if _, err := os.Stat(path); os.IsNotExist(err) {
+		err := os.MkdirAll(path, os.ModePerm)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	file, err := os.Create(path + "ica_host.log")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	InfoLogger = log.New(file, "INFO: ", log.Ldate|log.Ltime)
+	ErrorLogger = log.New(file, "ERROR: ", log.Ldate|log.Ltime|log.Lshortfile)
+}
+
+// Exported function for info logging
+func LogInfo(v ...interface{}) {
+	InfoLogger.Println(v...)
+}
+
+// Exported function for err logging
+func LogError(v ...interface{}) {
+	ErrorLogger.Println(v...)
+}
